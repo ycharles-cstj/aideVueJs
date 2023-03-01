c8d84914d3241f531a20c107b185256721ab3257
Yannick <ycharles@cstj.qc.ca>
2023-03-01 11:17:34 -0500

    feature-creation-composant

---
+++ src/components/exercice/CreationComposant/CreationComposant.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+    <div class="composant"> <!-- lorem20 -->
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut aperiam possimus voluptatem vel sapiente totam iste quasi quod, iusto reiciendis?</p>
+    </div>
+</template>
+
+<style lang="scss">
+    
+</style>
