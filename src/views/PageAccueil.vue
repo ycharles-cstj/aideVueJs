7de873425e74befa78d9561cc9df3f6356418ac9
Yannick <ycharles@cstj.qc.ca>
2023-03-01 01:19:52 -0500

    fix-table-matiere

--- src/views/PageAccueil.vue
+++ src/views/PageAccueil.vue
@@ -22,12 +22,19 @@
                 <li> <span>01</span> - Ajouter un composant</li>
                 <li> <span>02</span> - Ajouter une page et une nouvelle route</li>
                 <li> <span>03</span> - Ajouter un lien avec RouterLink</li>
-                <li> <span>04</span> - </li>
-                <li> <span>05</span> - </li>
-                <li> <span>06</span> - </li>
-                <li> <span>07</span> - </li>
-                <li> <span>08</span> - </li>
-                <li> <span>09</span> - </li>
+                <li> <span>04</span> - Création d'un sous-composant</li>
+                <li> <span>05</span> - Utilisation des interpolations</li>
+                <li> <span>06</span> - Data binding</li>
+                <li> <span>07</span> - Création des props</li>
+                <li> <span>08</span> - Utilisation des évènements</li>
+                <li> <span>09</span> - Utilisation des v-slot</li>
+                <li> <span>10</span> - Gestion des fonctions</li>
+                <li> <span>11</span> - Utilisation de stop </li>
+                <li> <span>12</span> - Utilisation des hook </li>
+                <li> <span>13</span> - Utilisation des styles conditionnels et v-show</li>
+                <li> <span>14</span> - Utilisation du v-model et des formulaires</li>
+                <li> <span>15</span> - Utilisation de v-for</li>
+                <li> <span>16</span> - Axios</li>
             </ul>
         </div>
 
