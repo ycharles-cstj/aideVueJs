a9d715bf75a6459478a4281f9b652dd5ada1a522
Yannick <ycharles@cstj.qc.ca>
2023-03-01 23:50:50 -0500

    feature-slots

---
+++ src/components/exercice/IntegrationSlot/IntegrationSlot.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+    <div class="slot">
+        <p class="exemple">Un composant utilisant des slots</p>
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="scss">
+    
+</style>
---
+++ src/components/exercice/IntegrationSlot/IntegrationSlotNomme.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+    <div class="slot">
+        <p class="exemple">Un composant utilisant des slots nommées</p>
+        <slot name="nom"></slot>
+        <slot name="info"></slot>
+    </div>
+</template>
+
+<style lang="scss">
+    
+</style>
